69228eb3492208b4ceb8559769b31945e6a65acb
EllisGL <xellisx@gmail.com>
2022-08-25 16:50:36 -0500

    Applying my highly opinionated formatting style.

--- src/spi.c
+++ src/spi.c
@@ -39,109 +39,99 @@
 static volatile u08 spiTransferComplete;
 
 // SPI interrupt service handler
-SIGNAL(SIG_SPI)
-{
-	spiTransferComplete = TRUE;
+
+SIGNAL(SIG_SPI) {
+    spiTransferComplete = TRUE;
 }
 #endif
 
 // access routines
-void spiInit()
-{
+
+void spiInit() {
 #ifdef __AVR_ATmega1281__
-	// setup SPI I/O pins
-	sbi(PORTB, 1);	// set SCK hi
-	sbi(DDRB, 1);	// set SCK as output
-	cbi(DDRB, 3);	// set MISO as input
-	sbi(DDRB, 2);	// set MOSI as output
-	sbi(DDRB, 0);	// SS must be output for Master mode to work
+    // setup SPI I/O pins
+    sbi(PORTB, 1); // set SCK hi
+    sbi(DDRB, 1); // set SCK as output
+    cbi(DDRB, 3); // set MISO as input
+    sbi(DDRB, 2); // set MOSI as output
+    sbi(DDRB, 0); // SS must be output for Master mode to work
 #elif __AVR_ATmega8__
     // setup SPI I/O pins
-    sbi(PORTB, 5);  // set SCK hi
-    sbi(DDRB, 5);   // set SCK as output
-    cbi(DDRB, 4);   // set MISO as input
-    sbi(DDRB, 3);   // set MOSI as output
-    sbi(DDRB, 2);   // SS must be output for Master mode to work
+    sbi(PORTB, 5); // set SCK hi
+    sbi(DDRB, 5); // set SCK as output
+    cbi(DDRB, 4); // set MISO as input
+    sbi(DDRB, 3); // set MOSI as output
+    sbi(DDRB, 2); // SS must be output for Master mode to work
 #else
 #pragma message( "No Matching device define for SPI configuration")
 #endif
 
-	// setup SPI interface :
-	// master mode
-	sbi(SPCR, MSTR);
-	// clock = f/4
-	// cbi(SPCR, SPR0);
-	// cbi(SPCR, SPR1);
-	// clock = f/16
-	sbi(SPCR, SPR0);
-	cbi(SPCR, SPR1);
-	//Select Mode 3 
-	sbi(SPCR, CPHA);
-	sbi(SPCR, CPOL);
-	// Data order LSB first
-	sbi(SPCR,DORD);
-	// enable SPI
-	sbi(SPCR, SPE);
-		
-	// clear status
-	inb(SPSR);
-	
+    // setup SPI interface :
+    // master mode
+    sbi(SPCR, MSTR);
+    // clock = f/4
+    // cbi(SPCR, SPR0);
+    // cbi(SPCR, SPR1);
+    // clock = f/16
+    sbi(SPCR, SPR0);
+    cbi(SPCR, SPR1);
+    //Select Mode 3 
+    sbi(SPCR, CPHA);
+    sbi(SPCR, CPOL);
+    // Data order LSB first
+    sbi(SPCR, DORD);
+    // enable SPI
+    sbi(SPCR, SPE);
 
-	// enable SPI interrupt
-	#ifdef SPI_USEINT
-	spiTransferComplete = TRUE;
-	sbi(SPCR, SPIE);
-	#endif
-}
-/*
-void spiSetBitrate(u08 spr)
-{
-	outb(SPCR, (inb(SPCR) & ((1<<SPR0)|(1<<SPR1))) | (spr&((1<<SPR0)|(1<<SPR1)))));
-}
-*/
-void spiSendByte(u08 data)
-{
-//	inb(SPSR);		// clear interrupt flag
-	outb(SPDR, data);	// This also clears the Interrupt flag 
-	
-	// send a byte over SPI and ignore reply
+    // clear status
+    inb(SPSR);
+
+
+    // enable SPI interrupt
 #ifdef SPI_USEINT
-		while(!spiTransferComplete);
-		spiTransferComplete = FALSE;
-#else
-		while(!(inb(SPSR) & (1<<SPIF)));
+    spiTransferComplete = TRUE;
+    sbi(SPCR, SPIE);
 #endif
-
-
 }
 
-u08 spiTransferByte(u08 data)
-{
+void spiSendByte(u08 data) {
+    //	inb(SPSR);		// clear interrupt flag
+    outb(SPDR, data); // This also clears the Interrupt flag 
+
+    // send a byte over SPI and ignore reply
 #ifdef SPI_USEINT
-	// send the given data
-	spiTransferComplete = FALSE;
-	outb(SPDR, data);
-	// wait for transfer to complete
-	while(!spiTransferComplete);
+    while (!spiTransferComplete);
+    spiTransferComplete = FALSE;
 #else
-	// send the given data
-	outb(SPDR, data);
-	// wait for transfer to complete
-	while(!(inb(SPSR) & (1<<SPIF)));
+    while (!(inb(SPSR) & (1 << SPIF)));
 #endif
-	// return the received data
-	return inb(SPDR);
 }
 
-u16 spiTransferWord(u16 data)
-{
-	u16 rxData = 0;
+u08 spiTransferByte(u08 data) {
+#ifdef SPI_USEINT
+    // send the given data
+    spiTransferComplete = FALSE;
+    outb(SPDR, data);
+    // wait for transfer to complete
+    while (!spiTransferComplete);
+#else
+    // send the given data
+    outb(SPDR, data);
+    // wait for transfer to complete
+    while (!(inb(SPSR) & (1 << SPIF)));
+#endif
+    // return the received data
+    return inb(SPDR);
+}
 
-	// send MS byte of given data
-	rxData = (spiTransferByte((data>>8) & 0x00FF))<<8;
-	// send LS byte of given data
-	rxData |= (spiTransferByte(data & 0x00FF));
+u16 spiTransferWord(u16 data) {
+    u16 rxData = 0;
 
-	// return the received data
-	return rxData;
+    // send MS byte of given data
+    rxData = (spiTransferByte((data >> 8) & 0x00FF)) << 8;
+    // send LS byte of given data
+    rxData |= (spiTransferByte(data & 0x00FF));
+
+    // return the received data
+    return rxData;
 }
