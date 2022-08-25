a818362b13ca6cffc23e0b20fc7bf9a66e1dfa52
EllisGL <xellisx@gmail.com>
2022-08-25 16:48:43 -0500

    Applying my highly opinionated formatting style.

--- src/serial.h
+++ src/serial.h
@@ -1,61 +1,60 @@
-/*
-  serial.c - Low level functions for sending and recieving bytes via the serial port
-  Part of Grbl v0.9
-
-  Copyright (c) 2012-2014 Sungeun K. Jeon
-
-  Grbl is free software: you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation, either version 3 of the License, or
-  (at your option) any later version.
-
-  Grbl is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
-
-  You should have received a copy of the GNU General Public License
-  along with Grbl.  If not, see <http://www.gnu.org/licenses/>.
-*/
-/* 
-  This file is based on work from Grbl v0.8, distributed under the 
-  terms of the MIT-license. See COPYING for more details.  
-    Copyright (c) 2009-2011 Simen Svale Skogsrud
-    Copyright (c) 2011-2012 Sungeun K. Jeon
-*/ 
-
+/**
+ * serial.c - Low level functions for sending and recieving bytes via the serial port
+ * Part of Grbl v0.9
+ *
+ * Copyright (c) 2012-2014 Sungeun K. Jeon
+ *
+ * Grbl is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Grbl is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Grbl.  If not, see <http://www.gnu.org/licenses/>.
+ */
+/** 
+ * This file is based on work from Grbl v0.8, distributed under the 
+ * terms of the MIT-license. See COPYING for more details.  
+ *   Copyright (c) 2009-2011 Simen Svale Skogsrud
+ *   Copyright (c) 2011-2012 Sungeun K. Jeon
+ */
 #ifndef serial_h
 #define serial_h
 
-#define BAUD_RATE      9600
+#define BAUD_RATE                   9600
 
 #define ENABLE_XONXOFF
 
-  #define UART1_RECEIVE_INTERRUPT   USART1_RX_vect
-  #define UART1_TRANSMIT_INTERRUPT  USART1_UDRE_vect
-  #define UART1_STATUS   UCSR1A
-  #define UART1_CONTROL  UCSR1B
-  #define UART1_DATA     UDR1
-  #define UART1_UDRIE    UDRIE1
+#define UART1_RECEIVE_INTERRUPT     USART1_RX_vect
+#define UART1_TRANSMIT_INTERRUPT    USART1_UDRE_vect
+#define UART1_STATUS                UCSR1A
+#define UART1_CONTROL               UCSR1B
+#define UART1_DATA                  UDR1
+#define UART1_UDRIE                 UDRIE1
 
 #ifndef RX_BUFFER_SIZE
-  #define RX_BUFFER_SIZE 128
+#define RX_BUFFER_SIZE              128
 #endif
 #ifndef TX_BUFFER_SIZE
-  #define TX_BUFFER_SIZE 64
+#define TX_BUFFER_SIZE              64
 #endif
 
-#define SERIAL_NO_DATA 0xff
+#define SERIAL_NO_DATA              0xFF
 
 #ifdef ENABLE_XONXOFF
-  #define RX_BUFFER_FULL 96 // XOFF high watermark
-  #define RX_BUFFER_LOW 32 // XON low watermark
-  #define SEND_XOFF 1
-  #define SEND_XON 2
-  #define XOFF_SENT 3
-  #define XON_SENT 4
-  #define XOFF_CHAR 0x13
-  #define XON_CHAR 0x11
+#define RX_BUFFER_FULL              96 // XOFF high watermark
+#define RX_BUFFER_LOW               32 // XON low watermark
+#define SEND_XOFF                   1
+#define SEND_XON                    2
+#define XOFF_SENT                   3
+#define XON_SENT                    4
+#define XOFF_CHAR                   0x13
+#define XON_CHAR                    0x11
 #endif
 
 void serial_init(void);
